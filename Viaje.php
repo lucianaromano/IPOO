759828df92da71d6b276543a87267643fd5c4071
lucianaromano <luciana.romano@fi.uncoma.edu.ar>
2023-04-18 22:15:38 -0300

    viaje

--- Viaje.php
+++ Viaje.php
@@ -61,6 +61,67 @@ public function getObjResponsableV(){
 public function setObjResponsableV($objResp){
   $this-> objResponsableV=$objResp;
 }
-}
+
+/**
+     * Permite saber si el dni ingresado es igual al de algun pasajero ya cargado y retorna un valor booleano.
+     * Agrega el nuevo pasajero al array con un indice anterior.
+     */
+    public function modificarDatos($codPasajero,$dniAnterior, $nombreNuevo, $apellidoNuevo,$teleNuevo){
+      $arregloPasajeros=$this->getArregloPasajeros();
+      $i=0;
+      $encontrado=false;
+
+      while ($i< count($arregloPasajeros) && !$encontrado){
+          $pasajero= $arregloPasajeros[$i];
+          $encontrado= ($pasajero->getDocumento() == $dniAnterior);  
+          $i++;  //return boolean
+      } if ($encontrado){  //no asigno un nuevo numero de dni a un pasajero
+          $newObjPasajero= new Pasajeros($nombreNuevo,$apellidoNuevo, $dniAnterior,$teleNuevo); //creo un nuevo obj pasajero
+          $arregloPasajeros[$i-1]= $newObjPasajero; 
+          $codPasajero= $this-> setArregloPasajeros($arregloPasajeros);
+      }
+      return $codPasajero;
+    }
+    /**
+     * Modifico la informacion del responsable del viaje
+     */
+    public function ModificarDatosResponsable($nroEmpleado, $nroLicencia, $nombre, $apellido){
+      $objResponsableV= $this-> getObjResponsableV();
+      $objResponsableV= new ResponsableV ($nroEmpleado, $nroLicencia, $nombre, $apellido); //creo un nuevo obj responsable del viaje
+
+      return $this->setObjResponsableV($objResponsableV);
+    }
+  
+     /**
+     * Muestra los datos de los pasajeros.
+     */
+    public function mostrarDatosPasajeros (){
+        $datosPasajeros=$this->getArregloPasajeros();
+        $pasajero="";
+        for ($i=0;$i<count($datosPasajeros);$i++){
+            $objPasajero=$datosPasajeros[$i];
+            $numPasajero=$i;
+            $pasajero= $pasajero ." ".$numPasajero."\n".
+                      $objPasajero."\n";
+        }
+        return $pasajero;
+    }
+/**
+     * Permite imprimir en pantalla los valores del viaje.
+     */
+    public function __toString(){
+      //$cadenaPasajeros= $this->mostrarDatosPasajeros();
+      $objResp=$this-> getObjResponsableV();
+      $cadena =   "Código del viaje: ".$this->getCodigo(). "\n".
+                  "Destino: ".$this->getDestino()."\n".
+                  "Cantidad máxima de pasajeros: " .$this->getCantidadMaximaPasajeros()."\n".
+                  "Responsable del viaje:" .$objResp->getNombre(). " " . $objResp->getApellido()."\n".
+                  "N° Empleado: ".$objResp->getNroEmpleado()."-"."N° Licencia: ".$objResp->getNroLicencia()."\n".
+                  "Datos pasajeros: \n" . $this->mostrarDatosPasajeros();
+      return $cadena;
+  }    
+
+    
+  }
 
 ?>
